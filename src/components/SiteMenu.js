d6919507b23704694d7023ae6be67d2ea8b82366
Eljoey <eljoeyy@gmail.com>
2020-08-25 11:26:23 -0500

    refactor

--- src/components/SiteMenu.js
+++ src/components/SiteMenu.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './Home';
 import Marketplace from './Marketplace';
-import UnderConstruction from './UnderConstruction';
 import CaphrasCalc from './CaphrasCalc';
 
 // TODO: Implement Region Selection.
