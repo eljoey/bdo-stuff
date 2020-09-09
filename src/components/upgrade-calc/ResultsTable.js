0b20529e6035ae49c4277036590a3b950b1d05a2
Eljoey <eljoeyy@gmail.com>
2020-09-09 09:16:35 -0500

    show more table data fro item upgrade info

--- src/components/upgrade-calc/ResultsTable.js
+++ src/components/upgrade-calc/ResultsTable.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Table } from 'react-materialize';
+import helpers from '../utils/helpers';
+import numeral from 'numeral';
 
 const ResultsTable = ({ data, sortBy }) => {
   const sortedData = data
@@ -13,16 +15,20 @@ const ResultsTable = ({ data, sortBy }) => {
           <th data-field='name'>Item Name</th>
           <th data-field='slot'>Slot</th>
           <th data-field='enhLevel'>Enhance Level</th>
-          <th data-field='cost'>{`Stat per Billion`}</th>
+          <th data-field='price'>Price</th>
+          <th data-field='statChange'>Stat Change</th>
+          <th data-field='cost'>Stat per Billion</th>
         </tr>
       </thead>
       <tbody>
         {sortedData.map((item) => (
           <tr>
-            <td>{item.name}</td>
+            <td className={helpers.getTextColor(item.grade)}>{item.name}</td>
             <td>{item.type}</td>
             <td>{item.enhLevel}</td>
-            <td>{item.perStatCost[sortBy]}</td>
+            <td>{numeral(item.price).format('($ 0.00 a)')}</td>
+            <td>{item.statChange[sortBy]}</td>
+            <td>{numeral(item.perStatCost[sortBy]).format('00.00')}</td>
           </tr>
         ))}
       </tbody>
