4a00cb35a303e90d5da6dacaa6842c314557cede
Eljoey <eljoeyy@gmail.com>
2020-10-13 13:29:22 -0500

    initial start

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'materialize-css/dist/css/materialize.min.css';
-import 'materialize-css/dist/js/materialize.min.js';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
